7753c00c7c8d21651cfb07632e5451f8c3d0983c
SANDHUZZ <vashishtsandhu@gmail.com>
2023-09-17 00:45:15 +0530

    installed react query . replaced useshowbyid with usequeryhook

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getShowById } from '../api/tvmaze';
+import ShowsGrid from '../components/SHOWS/ShowsGrid';
+
+// const useShowById = SHOWSID => {
+//   const [showData, setShowData] = useState(null);
+//   const [showError, setShowError] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const response = await getShowById(SHOWSID);
+//         setShowData(response);
+//       } catch (err) {
+//         setShowError(err);
+//       }
+//     }
+//     fetchData();
+//   }, [SHOWSID]);
+
+//   return { showData, showError };
+// };
 
 const Show = () => {
   const { SHOWSID } = useParams();
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', SHOWSID],
+    queryFn: () => getShowById(SHOWSID),
+  });
 
-  return <div>{`SHOWS PAGE FOR SHOW ID ${SHOWSID}`}</div>;
+  // const { showData, showError } = useShowById(SHOWSID);
+
+  if (showData) {
+    return <div>Got Show Data {showData.name}</div>;
+  }
+  if (showError) {
+    return <div>We have an error {showError.message}</div>;
+  }
+
+  return <div>Data is loading</div>;
 };
 export default Show;
